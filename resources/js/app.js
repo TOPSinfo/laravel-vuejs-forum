344a7ed9786d7e58a4149f6890a8458fd37a5278
nishit <nishit@topsinfosolutions.com>
2018-11-29 16:18:09 +0530

    Create a basic vue.js component

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,7 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('user-info', require('./components/UserInfo.vue'));
 
 // const files = require.context('./', true, /\.vue$/i)
 

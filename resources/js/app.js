58748fd5dc37c8c635a2c339e0f3153e73a975ec
nishit <nishit@topsinfosolutions.com>
2018-12-08 15:21:41 +0530

    Create Answers Component and implement vue list rendering

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,8 +26,8 @@ Vue.use(Authorization);
  */
 
 Vue.component('user-info', require('./components/UserInfo.vue'));
-Vue.component('answer', require('./components/Answer.vue'));
 Vue.component('vote', require('./components/Vote.vue'));
+Vue.component('answers', require('./components/Answers.vue'));
 
 // const files = require.context('./', true, /\.vue$/i)
 

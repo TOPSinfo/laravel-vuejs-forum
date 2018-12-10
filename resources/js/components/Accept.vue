b5747ef621f966451772129690dcbcd21da8881b
nishit <nishit@topsinfosolutions.com>
2018-12-10 12:43:23 +0530

    Fix issue on accept component by implementing event bus

--- resources/js/components/Accept.vue
+++ resources/js/components/Accept.vue
@@ -14,6 +14,8 @@
     </div>
 </template>
 <script>
+import EventBus from '../event-bus';
+
 export default {
     props: ['answer'],
 
@@ -22,6 +24,12 @@ export default {
             isBest: this.answer.is_best,
             id: this.answer.id
         }
+    },
+
+    created (){
+        EventBus.$on('accepted', id => {
+            this.isBest = (id === this.id);
+        });
     },
 
     methods: {
@@ -35,6 +43,8 @@ export default {
             });
 
             this.isBest = true;
+
+            EventBus.$emit('accepted', this.id);
         }
     },
 
